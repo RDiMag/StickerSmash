96386261c40f7b9d31f1e00a727e964de2874e38
Rachel DiMaggio <rdimaggio2010@gmail.com>
2025-05-04 11:53:27 -0400

    About and Not Found routes added

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View style={
-        styles.container
-      }
-    >
-      <Text style={styles.text}>Welcome!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -19,6 +20,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: 'ffff',
-  }
-})
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
